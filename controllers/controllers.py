07799d663120f3cd63242edcb426f101564207a8
nhanvu0901 <Neopet20001@gmail.com>
2022-10-10 15:45:50 +0700

    test2

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -8,34 +8,64 @@ from odoo.http import request
 
 from odoo import http
 
+import json
+import random
+import string
 
+import werkzeug
 
+from odoo import http
+from odoo.http import request
+import requests
 
+SCOPES = '''read_products,
+          read_customers,
+          write_customers,
+          read_third_party_fulfillment_orders,
+          write_third_party_fulfillment_orders,
+          read_orders,
+          write_orders,
+          write_draft_orders,
+          read_draft_orders,
+          write_script_tags,
+          read_shipping,
+          read_themes,
+          write_themes,
+          read_price_rules'''
 class TestShopi(http.Controller):
 
-
-
-    @http.route('/test_shopi', type='http', auth='none')
+    @http.route('/test_shopi', type='http', auth="public", website=True, method=['GET'], csrf=False)
     def index(self, **kw):
+
+        # if 'shop' in kw:
+        #     current_app = request.env.ref('app_external_id').sudo()
+        #     api_key = current_app.sp_api_key,
+        #     print(api_key)
         shopify.Session.setup(api_key="78e6382d4a5e2f2f37d5188204480b79", secret="0cc859187b6c7c7d82b1fd0e65683fd5")
         shop_url = "shoplify-odoo.myshopify.com"
         api_version = '2021-10'
         state = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
         redirect_uri = "http://localhost:8069/shopi"
-        scopes = ['read_products', 'read_orders']
+        scopes = ['read_products', "read_customers", "write_customers", "read_third_party_fulfillment_orders",
+                  "write_third_party_fulfillment_orders", "read_orders", "write_orders", "write_products",
+                  "write_draft_orders", "read_draft_orders", "write_script_tags",
+                  "read_shipping", "read_themes", "write_themes", "read_price_rules"]
 
         newSession = shopify.Session(shop_url, api_version)
         auth_url = newSession.create_permission_url(scopes, redirect_uri, state)
+
         return werkzeug.utils.redirect(auth_url)
 
     @http.route('/shopi', type='http', auth='none')
     def redirect(self, **kw):
-        shop_url = "shoplify-odoo.myshopify.com"
-        api_version = '2021-10'
+
+        shop_url = kw['shop']
+
+        api_version = '2021-07'
         session = shopify.Session(shop_url, api_version)
 
         access_token = session.request_token(request.params)
-        print(access_token)
+
         new_session = shopify.Session(shop_url, api_version, access_token)
 
         shopify.ShopifyResource.activate_session(new_session)
@@ -45,26 +75,42 @@ class TestShopi(http.Controller):
         currency = shop.currency
         country_name = shop.country_name
         email = shop.email
-        print(domain+" "+currency+" "+country_name+" "+email+" ")
 
-        shopify_model = request.env['test_shopify']
-        shopify_search = request.env['test_shopify'].sudo().search([])
-        flag = False
+        shopify_model = request.env['s.sp.shop']
 
-        for i in shopify_search:
-            if(i.shop_name == shop.name):
-                flag = True
+        shopify_exist = request.env['s.sp.shop'].sudo().search([('domain', '=', shop_url)], limit=1)
 
+        if shopify_exist:
+            shopify_exist.write(
+                {
+                    "access_token": access_token,
+                    "shop_name": shop.name,
+                    "domain": domain,
+                    "currency": currency,
+                    "country_name": country_name,
+                    "email": email
+                }
+            )
 
-        if(flag == False):
-            shopify_model.create({
+        else:
+            shopify_exist = shopify_model.create({
                 "access_token": access_token,
                 "shop_name": shop.name,
                 "domain": domain,
                 "currency": currency,
                 "country_name": country_name,
-                "email": email
+                "email": email,
+                "shop_id": shop.id
             })
 
-        return "Hello, world"
+            # shop_app = request.env['s.sp.app'].sudo().create({
+            #     "sp_shop_id": shopify_exist,
+            #
+            # })
+            topic = 'orders/updated'
+            path = 'orders/create/%s' % shopify_exist.shop_id
+            # shopify_exist.add_webhook_to_shop(topic, path)
+            shopify_exist.add_webhook_to_shop(topic)
 
+        print(shopify.Webhook.find())
+        return "Hello, world"
--- models/shopify.py
+++ models/shopify.py
@@ -1,17 +1,37 @@
+import shopify
 
 from odoo import models, fields, api
+from odoo.tools.safe_eval import datetime
+
+
+
 
 
 class Shopify(models.Model):
-
-    _name = 'test_shopify'
+    _name = 's.sp.shop'
     _description = 'test_shopi.test_shopi'
+    _rec_name = 'shop_name'
 
-
-    email = fields.Char(string="Email" ,readonly=True)
+    email = fields.Char(string="Email", readonly=True)
     access_token = fields.Char(string="Access Token")
     domain = fields.Char(string="Domain")
     currency = fields.Char(string="Currency")
     country_name = fields.Char(string="Country name")
-    email = fields.Char(string="Email")
     shop_name = fields.Char(string="Shop name")
+    shop_id=  fields.Char(string="Shop ID")
+    def add_webhook_to_shop(self, topic_name):
+        self.ensure_one()
+        # full_path = '#base_url' + path
+        # use ngrok https test tren local
+        # print(path)
+        full_path = 'https://9dcd-222-252-29-196.ap.ngrok.io'
+        webhook = shopify.Webhook()
+        webhook.topic = topic_name
+        webhook.address = full_path
+        webhook.format = 'json'
+        webhook.save()
+
+
+
+
+
